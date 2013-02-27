e79ddcbd425c5d05750b68c383c15316d98ba0ce
guoxx <guoxx@me.com>
2013-02-27 18:06:08 +0800

    add auto-complete 1.3.1

---
+++ init/auto-cmpl.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/packages/auto-complete/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/packages/auto-complete//ac-dict")
+(ac-config-default)
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
