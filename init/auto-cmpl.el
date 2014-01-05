318ffc0c24041632801f06fa677e6aca5707e095
guoxx <guoxx@me.com>
2014-01-05 15:12:34 +0800

    customize bootstrap variables

--- init/auto-cmpl.el
+++ init/auto-cmpl.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/packages/auto-complete/")
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/packages/auto-complete/ac-dict")
+(setq ac-comphist-file "~/.emacs.d/ignore-files/ac-comphist.dat")
 (ac-config-default)
 
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
--- init/bootstrap.el
+++ init/bootstrap.el
@@ -1,1 +1,3 @@
-
+(setq save-place-file "~/.emacs.d/ignore-files/places")
+(setq semanticdb-default-save-directory "~/.emacs.d/ignore-files/semanticdb")
+(setq bookmark-default-file "~/.emacs.d/ignore-files/bookmarks")
