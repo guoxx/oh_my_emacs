ce784c14b04967396f23694d61d732c0a02cca41
guoxx <guoxx@me.com>
2014-03-31 23:59:20 +0800

    x

--- init/bootstrap.el
+++ init/bootstrap.el
@@ -1,5 +1,7 @@
 (setq save-place-file (concat userdata-dir "places"))
 (setq semanticdb-default-save-directory (concat userdata-dir "semanticdb"))
 (setq bookmark-default-file (concat userdata-dir "bookmarks"))
+
+(require 'projectile)
 (setq projectile-known-projects-file (concat userdata-dir "projectile-bookmarks.eld"))
 (setq projectile-cache-file (concat userdata-dir "projectile.cache"))
