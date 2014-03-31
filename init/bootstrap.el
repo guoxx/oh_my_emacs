c79c895fe8dd2557a1ebbc9d0fed0ec77335eec3
guoxx <guoxx@me.com>
2014-03-31 23:53:27 +0800

    move projectile file to userdata directory

--- init/bootstrap.el
+++ init/bootstrap.el
@@ -1,3 +1,5 @@
 (setq save-place-file (concat userdata-dir "places"))
 (setq semanticdb-default-save-directory (concat userdata-dir "semanticdb"))
 (setq bookmark-default-file (concat userdata-dir "bookmarks"))
+(setq projectile-known-projects-file (concat userdata-dir "projectile-bookmarks.eld"))
+(setq projectile-cache-file (concat userdata-dir "projectile.cache"))
