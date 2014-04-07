69048697567f370d975dc6b72729d68ea064e457
guoxx <guoxx@me.com>
2014-04-08 00:43:35 +0800

    add yasnippets

--- init/init.el
+++ init/init.el
@@ -260,3 +260,8 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+
+(yas/global-mode)
+(setq yas/root-directory "~/.emacs.d/snippets")
+(yas/load-directory yas/root-directory)
