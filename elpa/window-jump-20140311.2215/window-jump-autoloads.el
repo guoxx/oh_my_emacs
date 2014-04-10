d230e8fd1ca64efb4763df6bdfbeb48747cf9ddb
guoxx <guoxx@me.com>
2014-04-11 00:06:22 +0800

    add elpa packages to repository

---
+++ elpa/window-jump-20140311.2215/window-jump-autoloads.el
@@ -0,0 +1,45 @@
+;;; window-jump-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (window-jump-up window-jump-down window-jump-right
+;;;;;;  window-jump-left) "window-jump" "window-jump.el" (21317 29527
+;;;;;;  0 0))
+;;; Generated autoloads from window-jump.el
+
+(autoload 'window-jump-left "window-jump" "\
+Move to the window to the left of the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-right "window-jump" "\
+Move to the window to the right of the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-down "window-jump" "\
+Move to the window below the current window.
+
+\(fn)" t nil)
+
+(autoload 'window-jump-up "window-jump" "\
+Move to the window above the current window.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("window-jump-pkg.el") (21317 29527 475426
+;;;;;;  0))
+
+;;;***
+
+(provide 'window-jump-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; window-jump-autoloads.el ends here
---
+++ elpa/window-jump-20140311.2215/window-jump-pkg.el
@@ -0,0 +1,1 @@
+(define-package "window-jump" "20140311.2215" "Move left/right/up/down through your windows." (quote nil))
