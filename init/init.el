255a5db2553a2631c3aac5b0bc83a97c729125ab
guoxx <guoxx@me.com>
2014-04-14 22:42:58 +0800

    x

--- init/init.el
+++ init/init.el
@@ -238,3 +238,21 @@
 
 (desktop-save-mode 1)
 (setq desktop-path '("~/.emacs.d/userdata/"))
+
+
+(defun ibuffer-ido-find-file (file &optional wildcards)
+  "Like `ido-find-file', but default to the directory of the buffer at point."
+  (interactive
+   (let ((default-directory
+           (let ((buf (ibuffer-current-buffer)))
+             (if (buffer-live-p buf)
+                 (with-current-buffer buf
+                   default-directory)
+               default-directory))))
+     (list (ido-read-file-name "Find file: " default-directory) t)))
+  (find-file file wildcards))
+
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (define-key ibuffer-mode-map "\C-x\C-f"
+              'ibuffer-ido-find-file)))
