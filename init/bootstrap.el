54239786a32a79de3e48e4b41f972f19b13c3a7a
ben <guoxx@me.com>
2014-08-02 19:54:34 +0800

    rename some variables

--- init/bootstrap.el
+++ init/bootstrap.el
@@ -1,10 +1,10 @@
-(setq save-place-file (concat userdata-dir "places"))
-(setq semanticdb-default-save-directory (concat userdata-dir "semanticdb"))
-(setq bookmark-default-file (concat userdata-dir "bookmarks"))
+(setq save-place-file (concat oh-my-emacs-userdata-dir "places"))
+(setq semanticdb-default-save-directory (concat oh-my-emacs-userdata-dir "semanticdb"))
+(setq bookmark-default-file (concat oh-my-emacs-userdata-dir "bookmarks"))
 
 (require 'projectile)
-(setq projectile-known-projects-file (concat userdata-dir "projectile-bookmarks.eld"))
-(setq projectile-cache-file (concat userdata-dir "projectile.cache"))
+(setq projectile-known-projects-file (concat oh-my-emacs-userdata-dir "projectile-bookmarks.eld"))
+(setq projectile-cache-file (concat oh-my-emacs-userdata-dir "projectile.cache"))
 
 (setq use-empty-active-region 't)
 
